9cf3a0ef21e52dd34377635f53ffea4f8f16c418
Maksim Pischulenok <pischule@outlook.com>
2022-08-15 23:47:03 +0300

    add button command

--- backend/lib/bot.js
+++ backend/lib/bot.js
@@ -1,10 +1,10 @@
-import {Telegraf} from 'telegraf';
-import {config} from '../config/config.js';
-import * as util from './util.js';
-import {readObj, saveObj, readMenu} from './storage.js';
-import {createTaskAndGetDocument} from './ocrsdk.js';
-import {parseDocument} from './parser.js';
-import {escapeMarkdown} from './util.js';
+import { Telegraf } from "telegraf";
+import { config } from "../config/config.js";
+import * as util from "./util.js";
+import { readObj, saveObj, readMenu } from "./storage.js";
+import { createTaskAndGetDocument } from "./ocrsdk.js";
+import { parseDocument } from "./parser.js";
+import { escapeMarkdown } from "./util.js";
 
 const RECTS_REGEX = /.*http.*\?r=([\d.]+).*?/;
 const DATE_REGEX = /.*?меню на.*?(\d{1,2}\.\d{1,2}\.\d{2,4}).*/i;
@@ -15,21 +15,21 @@ const bot = new Telegraf(config.botToken);
 
 bot.use(adminOnlyMiddleware);
 
-bot.on('photo', async (ctx) => {
-  console.log('received photo');
+bot.on("photo", async (ctx) => {
+  console.log("received photo");
   try {
     if (ctx.chat.id === config.ordersChat && !isOnTime()) {
-      console.log('received photo too late');
+      console.log("received photo too late");
       return;
     }
 
-    await ctx.replyWithChatAction('typing');
-    const rects = await readObj('rects');
+    await ctx.replyWithChatAction("typing");
+    const rects = await readObj("rects");
     if (rects == null) {
       if (ctx.chat.id !== config.ordersChat) {
-        ctx.reply('no rects');
+        ctx.reply("no rects");
       }
-      console.error('received photo while not having rects');
+      console.error("received photo while not having rects");
       return;
     }
 
@@ -50,33 +50,23 @@ bot.on('photo', async (ctx) => {
       deliveryDate.setDate(deliveryDate.getDate() + 1);
     }
 
-    await saveObj('menu', {
-      items, createDate, deliveryDate: deliveryDate,
+    await saveObj("menu", {
+      items,
+      createDate,
+      deliveryDate: deliveryDate,
     });
-    console.log('save new menu from photo');
+    console.log("save new menu from photo");
 
-    const button = {
-      text: 'Создать заказ', login_url: {
-        url: config.appUrl,
-      },
-    };
-
-    ctx.reply('Нажмите на кнопку ниже, чтобы создать заказ', {
-      reply_markup: {
-        inline_keyboard: [
-          [
-            button]],
-      },
-    });
+    publishButton(ctx);
   } catch (err) {
-    console.error('photo parsing failed', err);
+    console.error("photo parsing failed", err);
   }
 });
 
-bot.command('rects', async (ctx) => {
-  console.log('rects called');
+bot.command("rects", async (ctx) => {
+  console.log("rects called");
   try {
-    const rects = await readObj('rects');
+    const rects = await readObj("rects");
     const rectsString = util.rectsToString(rects);
     ctx.reply(`${config.rectsAppUrl}?r=${rectsString}`);
   } catch (err) {
@@ -86,11 +76,11 @@ bot.command('rects', async (ctx) => {
 });
 
 bot.hears(RECTS_REGEX, async (ctx) => {
-  console.log('heard rects regex');
+  console.log("heard rects regex");
   try {
     const receivedRectsString = ctx.message.text.match(RECTS_REGEX)[1];
     const rects = util.stringToRects(receivedRectsString);
-    await saveObj('rects', rects);
+    await saveObj("rects", rects);
     ctx.reply(`${config.rectsAppUrl}?r=${receivedRectsString}`);
   } catch (err) {
     console.error(err);
@@ -98,10 +88,10 @@ bot.hears(RECTS_REGEX, async (ctx) => {
   }
 });
 
-bot.command('menu', async (ctx) => {
-  console.log('menu called');
+bot.command("menu", async (ctx) => {
+  console.log("menu called");
   try {
-    const lines = ctx.message.text.split('\n');
+    const lines = ctx.message.text.split("\n");
     if (lines.length > 1) {
       lines.shift();
       const items = lines.map((line) => line.trim()).filter((line) => line);
@@ -110,11 +100,11 @@ bot.command('menu', async (ctx) => {
         deliveryDate = new Date(createDate.getTime());
         deliveryDate.setDate(deliveryDate.getDate() + 1);
       }
-      await saveObj('menu', {items, deliveryDate, createDate});
+      await saveObj("menu", { items, deliveryDate, createDate });
     }
 
     const menu = await readMenu();
-    ctx.reply('/menu\n' + menu.items.join('\n'));
+    ctx.reply("/menu\n" + menu.items.join("\n"));
   } catch (err) {
     console.error(err);
     ctx.reply(`error: ${err}`);
@@ -122,42 +112,65 @@ bot.command('menu', async (ctx) => {
 });
 
 bot.hears(DATE_REGEX, (ctx) => {
-  console.log('heard date regex');
+  console.log("heard date regex");
   try {
     const dateString = ctx.message.text.match(DATE_REGEX)[1];
-    const [day, month, year] = dateString.split('.');
+    const [day, month, year] = dateString.split(".");
     deliveryDate = new Date(+year, month - 1, +day);
   } catch (err) {
     console.error(err);
   }
 });
 
-bot.command('del', (ctx) => {
+bot.command("del", (ctx) => {
   const reply = ctx.message?.reply_to_message;
   if (!reply || reply.from.id !== ctx.botInfo.id) {
     return;
   }
   ctx.deleteMessage(reply.message_id);
-})
+});
+
+bot.command("button", (ctx) => {
+  publishButton(ctx);
+});
+
+function publishButton(ctx) {
+  const button = {
+    text: "Создать заказ",
+    login_url: {
+      url: config.appUrl,
+    },
+  };
+
+  ctx.reply("Нажмите на кнопку ниже, чтобы создать заказ", {
+    reply_markup: {
+      inline_keyboard: [[button]],
+    },
+  });
+}
 
 function publishOrder(order, userId) {
-  console.log('publish order', userId, order);
+  console.log("publish order", userId, order);
   try {
     const mention = `[${util.escapeMarkdown(
-        order.name)}](tg://user?id=${userId})`;
-    const items = order.items.map(item => {
-      if (item.quantity === 1) {
-        return `- ${item.name}`;
-      } else {
-        return `- ${item.name} x${item.quantity}`;
-      }
-    }).join('\n');
+      order.name
+    )}](tg://user?id=${userId})`;
+    const items = order.items
+      .map((item) => {
+        if (item.quantity === 1) {
+          return `- ${item.name}`;
+        } else {
+          return `- ${item.name} x${item.quantity}`;
+        }
+      })
+      .join("\n");
     const message = `${mention}:\n${escapeMarkdown(items)}`;
     return bot.telegram.sendMessage(config.ordersChat, message, {
-      parse_mode: 'MarkdownV2',
+      parse_mode: "MarkdownV2",
     });
   } catch (err) {
     console.error(err);
+    throw err;
   }
 }
 
@@ -168,9 +181,11 @@ function adminOnlyMiddleware(ctx, next) {
 }
 
 function isOnTime() {
-  const hourAtMinsk = util.convertTZ(new Date(), 'Europe/Minsk').getHours();
-  return config.acceptPhotoFromHour >= hourAtMinsk && hourAtMinsk <
-      config.acceptPhotoToHour;
+  const hourAtMinsk = util.convertTZ(new Date(), "Europe/Minsk").getHours();
+  return (
+    config.acceptPhotoFromHour >= hourAtMinsk &&
+    hourAtMinsk < config.acceptPhotoToHour
+  );
 }
 
-export {publishOrder, bot};
+export { publishOrder, bot };
